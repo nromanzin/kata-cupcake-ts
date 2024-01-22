15a3cdc34bedce3de4f4093c174cb0aed40120b8
nromanzin <nicolas.romanzin@gmail.com>
2024-01-22 10:42:29 +0100

    write 1st failing test

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,7 +1,8 @@
 import { add } from './kata';
 
 describe('Kata', () => {
-  test('should get 4 when adding 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+  test('The name function should return “🧁”', () => {
+    const cupcake = new Cupcake();
+    expect(cupcake.name()).toBe('🧁');
   });
 });
